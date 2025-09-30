43cddd214cb3555064d4248ead551a13a3e04954
teplostanski <contact@teplostanski.me>
2025-09-30 09:12:24 +0000

    test: empty string header value is present

--- test/headers.test.ts
+++ test/headers.test.ts
@@ -5,8 +5,8 @@ describe("normalizeHeaders", () => {
   it("lowercases keys", () => {
     expect(normalizeHeaders({ "X-Foo": "1" })).toEqual({ "x-foo": "1"   it("returns true when present", () => {
     expect(hasHeader({ ETag: "abc" }, "etag")).toBe(true);
-    it("normalizes mixed-case keys", () => {
-    expect(normalizeHeaders({ "X-A": "1", "x-a": "2" })).toEqual({ "x-a": "2" });
+    it("treats empty string as present", () => {
+    expect(hasHeader({ "x": "" }, "X")).toBe(true);
   });
 });
 });
